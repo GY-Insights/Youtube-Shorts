dd5d3442f2d2eeafe8d43cafc2df3f9b68253228
GY Insights <148642886+GY-Insights@users.noreply.github.com>
2023-12-06 14:39:43 +0530

    Create dutch-national-flag.cpp

---
+++ dutch-national-flag.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void sort012(vector<int> &arr){
+    int i =0, j= 0, k = arr.size() - 1;
+
+    while(i <= k){
+        if(arr[i] == 0){
+            swap(arr[i], arr[j]);
+            i++;
+            j++;
+        }
+        else if(arr[i] == 1){
+            i++;
+        }
+        else{
+            swap(arr[i], arr[k]);
+            k--;
+        }
+    }
+}
+
+void display(vector<int> arr){
+    for(int i = 0; i < arr.size(); i++){
+        cout<<arr[i]<<" ";
+    }
+}
+
+
+int main(){
+    
+    vector<int> arr = {0,1,2,0,1,2,1,2,0,0,1,1,2,0,2};
+    sort012(arr);
+    display(arr);
+    return 0;
+}
